8b1a4ba3ad2ad26ed58cf5465ed78b256413c7f1
ekanata14 <ekanata1411@gmail.com>
2024-01-23 00:20:49 +0800

    Make login api

--- back-end/server.js
+++ back-end/server.js
@@ -2,10 +2,14 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const userRoutes = require('./src/routes/userRoutes');
 const messageRoutes = require('./src/routes/messagesRoutes');
+const userModels = require('./models/users');
 const {Sequelize, DataTypes} = require('sequelize');
 const app = express()
+const jwt = require('jsonwebtoken');
+
 require('dotenv').config();
-const port = process.env.PORT
+const port = process.env.PORT;
+const secretKey = process.env.SECRET_KEY;
 
 const sequelize = new Sequelize({
   dialect: 'mysql',
@@ -16,6 +20,24 @@ const sequelize = new Sequelize({
 });
 
 app.use(bodyParser.json());
+
+app.post('/login', async(req, res) => {
+  const {username, password} = req.body;
+
+  try{
+    const user = await sequelize.query(`SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`);
+    if(!user){
+      return res.status(401).json({message: "Invalid Credentials"});
+    }
+
+    const token = jwt.sign({userId: user.id, username: user.username}, secretKey, {expiresIn: '1h'});
+
+    res.json(token);
+  } catch(error){
+    console.error(error);
+    res.status(500).json({message: "Internal Server Error"});
+  }
+});
 
 app.use('/users', userRoutes);
 app.use('/message', messageRoutes);
